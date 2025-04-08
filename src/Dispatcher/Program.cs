c4069ba368a2c1182d638ce9b5706c0c0ed2f813
Brandon Ros <brandonros1@gmail.com>
2025-04-07 21:40:04 -0400

    test

--- src/Dispatcher/Program.cs
+++ src/Dispatcher/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration
     .SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}2.json", optional: true, reloadOnChange: true)
     .AddEnvironmentVariables();
 
 // Add services to the container.
